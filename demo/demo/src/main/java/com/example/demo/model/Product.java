4994fb941c239c2e4fcd3940b7a0a951d2e4b6d1
spapowW <uirandonnee@gmail.com>
2024-11-16 19:26:37 +0100

    issam1

--- demo/demo/src/main/java/com/example/demo/model/Product.java
+++ demo/demo/src/main/java/com/example/demo/model/Product.java
@@ -1,12 +1,8 @@
 package com.example.demo.model;
-
 import jakarta.persistence.*;
-
-
 @Entity
 @Table(name ="products")
 public class Product {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
