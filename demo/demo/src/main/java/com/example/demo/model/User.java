212a37f7435417326bceee449d2bcbf0b7031042
spapowW <uirandonnee@gmail.com>
2024-11-16 17:41:27 +0100

    issam1

--- demo/demo/src/main/java/com/example/demo/model/User.java
+++ demo/demo/src/main/java/com/example/demo/model/User.java
@@ -1,4 +1,55 @@
 package com.example.demo.model;
-
+import jakarta.persistence.*;
+@Entity
+@Table(name ="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String nom;
+    @Column
+    private String email;
+    @Column
+    private String password;
+    public User() {
+    }
+    public User(String nom, String email, String password) {
+        this.nom = nom;
+        this.email = email;
+        this.password = password;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
