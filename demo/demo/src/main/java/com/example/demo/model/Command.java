de186306c00c7150635a6fd166b7618a3fb7300f
yassine <yassine.benhari5@gmail.com>
2024-11-17 20:23:51 +0100

    yassine change

--- demo/demo/src/main/java/com/example/demo/model/Command.java
+++ demo/demo/src/main/java/com/example/demo/model/Command.java
@@ -1,14 +1,12 @@
 package com.example.demo.model;
 
-
 import jakarta.persistence.*;
-
+import java.time.LocalDateTime;
 import java.util.List;
-
-
 
 @Entity
 public class Command {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id_command;
@@ -20,12 +18,14 @@ public class Command {
     @OneToMany(mappedBy = "command", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Product> products;
 
+    private LocalDateTime creationDate;
+
     // Getters et setters
     public int getId_command() {
         return id_command;
     }
 
-    public void setId(int id_command) {
+    public void setId_command(int id_command) {
         this.id_command = id_command;
     }
 
@@ -45,13 +45,23 @@ public class Command {
         this.products = products;
     }
 
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    // Constructeurs
     public Command() {
     }
 
-    public Command(int id_command, User user, List<Product> products) {
+    public Command(int id_command, User user, List<Product> products, LocalDateTime creationDate) {
         this.id_command = id_command;
         this.user = user;
         this.products = products;
+        this.creationDate = creationDate;
     }
 
     @Override
@@ -60,7 +70,7 @@ public class Command {
                 "id_command=" + id_command +
                 ", user=" + user +
                 ", products=" + products +
+                ", creationDate=" + creationDate +
                 '}';
     }
 }
-
